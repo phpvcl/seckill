8943f3fe5b950a2fa30cc26a1fa0adaff0bd6894
ant <95570971@qq.com>
2018-08-23 20:50:23 -0400

    整理一上代码

--- app/controller/Crontab.php
+++ app/controller/Crontab.php
@@ -9,11 +9,30 @@ namespace app\controller;
 
 class Crontab
 {
+    //消息频道
     private $channel = 'orderChannel';
+    //redis连接对象
+    private $redis = null;    
     //redis IP
     private $host = '127.0.0.1';
     //redis 端口
     private $port = '6379';    
+    public function __construct()
+    {
+        try
+        {
+            if (!class_exists('Redis'))
+            {
+                throw new \Exception('Redis扩展不存在');
+            }
+            $this->redis = new \Redis();
+            $this->redis->pconnect($this->host, $this->port, 0);
+        }
+        catch (\Exception $e)
+        {
+            die($e->getMessage());
+        }
+    }    
     /**
      * 订阅消息
      * 
@@ -21,9 +40,7 @@ class Crontab
      */
     public function execute()
     {
-        $redis = new \Redis();
-        $redis->pconnect($this->host, $this->port, 0);
-        $redis->subscribe(array($this->channel), array($this, 'callback'));
+        $this->redis->subscribe(array($this->channel), array($this, 'callback'));
     }
     /**
      * 接到消息后，回调处理
@@ -33,6 +50,7 @@ class Crontab
     public function callback($redis, $channelName, $message)
     {
         echo $channelName, "==>", $message, PHP_EOL;
+        
         //这里可以将队列保存进数据库
     }
 
